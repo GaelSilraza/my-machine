32fd283f10da031d34f3ca578cd8c2e35b0e54cb
Gabriel da Silveira Souza <gabrielss1298@hotmail.com>
2024-11-01 22:49:19 -0300

    feat: start

---
+++ important-files.md
@@ -0,0 +1,9 @@
+# Interface
+
+## My favorite gnome extensions
+
+[Dash to panel](https://extensions.gnome.org/extension/1160/dash-to-panel/)
+
+[AppIndicator and KStatusNotifierItem Support](https://extensions.gnome.org/extension/615/appindicator-support/)
+
+If it wasn't installed by default, install `gnome tweaks`
---
+++ start-basic-apps.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install libpq-dev \
+  python3-dev \
+  python3-venv \
+  build-essential \
+  libpcre3-dev \
+  curl \
+  nano \
+  ca-certificates \
+  nmap \
+  flameshot -y  
+sudo apt clean -y
+
+sudo apt install flatpak gnome-software-plugin-flatpak  -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.calibre_ebook.calibre
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.sublimetext.three
+flatpak install flathub io.beekeeperstudio.Studio
+flatpak install flathub net.ankiweb.Anki
+flatpak install flathub org.kde.kolourpaint
+flatpak install flathub rest.insomnia.Insomnia
+flatpak install flathub org.telegram.desktop
+
+flatpak override --env=GTK_THEME=Adwaita:dark com.calibre_ebook.calibre
+flatpak override --env=GTK_THEME=Adwaita:dark com.discordapp.Discord
+flatpak override --env=GTK_THEME=Adwaita:dark com.sublimetext.three
+flatpak override --env=GTK_THEME=Adwaita:dark io.beekeeperstudio.Studio
+flatpak override --env=GTK_THEME=Adwaita:dark net.ankiweb.Anki
+flatpak override --env=GTK_THEME=Adwaita:dark org.kde.kolourpaint
+flatpak override --env=GTK_THEME=Adwaita:dark rest.insomnia.Insomnia
+flatpak override --env=GTK_THEME=Adwaita:dark org.telegram.desktop
+
+snap install spotify
+sudo snap install code --classic
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.bashrc
+
+sudo apt install psensor
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin -y
+sudo apt update -y
+sudo apt install docker-compose-plugin
